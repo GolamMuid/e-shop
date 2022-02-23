da58eb7127790ea6c361e01359aa5df44d007a52
GolamMuid <golammuid@gmail.com>
2022-02-23 20:55:15 +0600

    added script

---
+++ script.js
@@ -0,0 +1,54 @@
+// navbar sticky starts
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+//   Navbar sticky ends
+
+// deal of the day slider starts
+
+var swiper = new Swiper(".dotdSwiper", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// deal of the day slider ends
+
+// new arrival product slider starts
+
+var swiper = new Swiper(".napSwiper", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// new arrival product slider starts
