fc9d035a705dc0288bb98c6751d913e517f48b66
GolamMuid <golammuid@gmail.com>
2022-02-24 20:46:59 +0600

    added all categories and bottom nav

--- script.js
+++ script.js
@@ -17,6 +17,30 @@ function myFunction() {
 
 //   Navbar sticky ends
 
+// categories dropdown starts
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+// categories dropdown endss
+
 // deal of the day slider starts
 
 var swiper = new Swiper(".dotdSwiper", {
