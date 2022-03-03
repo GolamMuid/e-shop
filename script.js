acbd73ac6ee6cedb37015b0278c4dc67248f5140
GolamMuid <golammuid@gmail.com>
2022-03-03 13:17:28 +0600

    added category detail page

--- script.js
+++ script.js
@@ -180,7 +180,44 @@ function clickme(smallImg) {
   fullImg.src = smallImg.src;
 }
 
-//product detail script starts
+//product detail script ends
+
+// range slider starts
+
+window.onload = function () {
+  slideOne();
+  slideTwo();
+};
+
+let sliderOne = document.getElementById("slider-1");
+let sliderTwo = document.getElementById("slider-2");
+let displayValOne = document.getElementById("range1");
+let displayValTwo = document.getElementById("range2");
+let minGap = 0;
+let sliderTrack = document.querySelector(".slider-track");
+let sliderMaxValue = document.getElementById("slider-1").max;
+
+function slideOne() {
+  if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+    sliderOne.value = parseInt(sliderTwo.value) - minGap;
+  }
+  displayValOne.textContent = sliderOne.value;
+  fillColor();
+}
+function slideTwo() {
+  if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+    sliderTwo.value = parseInt(sliderOne.value) + minGap;
+  }
+  displayValTwo.textContent = sliderTwo.value;
+  fillColor();
+}
+function fillColor() {
+  percent1 = (sliderOne.value / sliderMaxValue) * 100;
+  percent2 = (sliderTwo.value / sliderMaxValue) * 100;
+  sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3c7a17 ${percent1}% , #3c7a17 ${percent2}%, #dadae5 ${percent2}%)`;
+}
+
+// range slider ends
 
 //change password
 
